5581bc290217f5eb4e66ec149e5f8847afc5f4bd
Ákos Gángoly <gangoly96@gmail.com>
2020-03-31 21:11:49 +0000

    bugfix

--- run_less_client.py
+++ run_less_client.py
@@ -6,7 +6,11 @@ from SyftFederatedLearnerMNIST import (
 )
 
 
-def train(experiment, config):
+def train(l):
+    name = l[0]
+    config = l[1]
+    experiment = Experiment(workspace="federated-learning", project_name="less_client")
+    experiment.set_name(name)
     learner = SyftFederatedLearnerMNIST(experiment, config)
     learner.train()
 
@@ -14,17 +18,13 @@ def train(experiment, config):
 C = 1
 E = 5
 B = 50
-experiments = []
+names = []
 configs = []
 for is_iid in [False, True]:
     for NC in [1, 2, 3, 4, 5, 10, 20, 50, 100]:
         dist = "IID" if is_iid else "non IID"
         name = f"{dist} - {NC}"
 
-        experiment = Experiment(
-            workspace="federated-learning", project_name="less_client"
-        )
-        experiment.set_name(name)
         # TODO a paraméterek helytelen nevére nem adott hibát
         config = SyftFederatedLearnerMNISTConfig(
             LEARNING_RATE=0.1,
@@ -34,9 +34,10 @@ for is_iid in [False, True]:
             N_CLIENTS=NC,
             N_EPOCH_PER_CLIENT=E,
             MAX_ROUNDS=1500,
+            DL_N_WORKER=0,
         )
-        experiments.append(experiment)
+        names.append(name)
         configs.append(config)
 
 with mp.Pool(2) as p:
-    p.map(train, zip(experiments, configs))
+    p.map(train, list(zip(names, configs)))
