cf63c5fdc6bf0ff70eef30b13402c2a795c3d7b0
Ákos Gángoly <gangoly96@gmail.com>
2020-04-12 18:44:47 +0000

    long run

--- run_2C_opt.py
+++ run_2C_opt.py
@@ -17,19 +17,21 @@ C = 1
 NC = 2
 E = 1
 B = 50
+opt_strategy = "nothing"
 is_iid = False
-for lr in [0.0001, 1, 0.00001, 10]:
+for lr in [0.001, 0.01, 0.1, 0.0001, 1, 0.00001, 10]:
     for opt in TorchOptRepo.get_opt_names():
-        name = f"{opt} - {lr} - {E}"
+        name = f"{opt} - {opt_strategy} - {lr} - {E}"
 
         logging.info(name)
         experiment = Experiment(workspace="federated-learning", project_name="2C_opt")
         experiment.set_name(name)
-        experiment.log_parameter("opt_srategy", "reinit")
+        experiment.log_parameter("opt_srategy", opt_strategy)
         # TODO a paraméterek helytelen nevére nem adott hibát
         config = TorchFederatedLearnerMNISTConfig(
             LEARNING_RATE=lr,
             OPT=opt,
+            OPT_STRATEGY=opt_strategy,
             IS_IID_DATA=is_iid,
             BATCH_SIZE=B,
             CLIENT_FRACTION=C,
