0182038aaef22b9329b2054cdd6f1b0da16e5907
Ákos Gángoly <gangoly96@gmail.com>
2020-05-04 16:32:50 +0000

    run the new arch with avg

--- run_2C_opt.py
+++ run_2C_opt.py
@@ -15,31 +15,31 @@ logging.basicConfig(
 
 C = 1
 NC = 2
-E = 5
+E = 1
 B = 50
 opt_strategy = "avg"
 is_iid = False
 lr = 0.01
 opt = "Rprop"
-# for lr in [0.01, 0.1, 0.0001, 1, 0.00001, 10]:
-# for opt in TorchOptRepo.get_opt_names():
-name = f"keep - {opt} - {opt_strategy} - {lr} - {E}"
+for lr in [0.01, 0.1, 0.0001, 1, 0.00001, 10]:
+    for opt in TorchOptRepo.get_opt_names():
+        name = f"noKeep - {opt} - {opt_strategy} - {lr} - {E}"
 
-logging.info(name)
-experiment = Experiment(workspace="federated-learning", project_name="2C_opt")
-experiment.set_name(name)
-experiment.log_parameter("opt_srategy", opt_strategy)
-# TODO a paraméterek helytelen nevére nem adott hibát
-config = TorchFederatedLearnerMNISTConfig(
-    LEARNING_RATE=lr,
-    OPT=opt,
-    OPT_STRATEGY=opt_strategy,
-    IS_IID_DATA=is_iid,
-    BATCH_SIZE=B,
-    CLIENT_FRACTION=C,
-    N_CLIENTS=NC,
-    N_EPOCH_PER_CLIENT=E,
-    MAX_ROUNDS=2,
-)
-learner = TorchFederatedLearnerMNIST(experiment, config)
-learner.train()
+        logging.info(name)
+        experiment = Experiment(workspace="federated-learning", project_name="2C_opt")
+        experiment.set_name(name)
+        experiment.log_parameter("opt_srategy", opt_strategy)
+        # TODO a paraméterek helytelen nevére nem adott hibát
+        config = TorchFederatedLearnerMNISTConfig(
+            LEARNING_RATE=lr,
+            OPT=opt,
+            OPT_STRATEGY=opt_strategy,
+            IS_IID_DATA=is_iid,
+            BATCH_SIZE=B,
+            CLIENT_FRACTION=C,
+            N_CLIENTS=NC,
+            N_EPOCH_PER_CLIENT=E,
+            MAX_ROUNDS=100,
+        )
+        learner = TorchFederatedLearnerMNIST(experiment, config)
+        learner.train()
