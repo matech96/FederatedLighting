96c2f5a8fdf8fc84bf4b5561d26869d7e00f67f2
Ákos Gángoly <gangoly96@gmail.com>
2020-06-22 06:46:15 +0000

    extended no groupnorm

--- FLF/TorchFederatedLearnerCIFAR100.py
+++ FLF/TorchFederatedLearnerCIFAR100.py
@@ -99,9 +99,10 @@ class TorchFederatedLearnerCIFAR100(TorchFederatedLearner):
         return train_loader_list
 
     def get_model_cls(self) -> Callable[[], nn.Module]:
-        make_group_norm = lambda x: th.nn.GroupNorm(2, x)
-        make_model = lambda: models.resnet18(norm_layer=make_group_norm)
-        return make_model, False
+        # make_group_norm = lambda x: th.nn.GroupNorm(2, x)
+        # make_model = lambda: models.resnet18(norm_layer=make_group_norm)
+        # return make_model, False
+        return models.resnet18, False
 
     def get_loss(self):
         return nn.CrossEntropyLoss()
