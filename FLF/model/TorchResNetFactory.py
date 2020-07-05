f3181de5df07a6ff139d267be1614394fa3d416f
Ákos Gángoly <gangoly96@gmail.com>
2020-07-05 16:33:41 +0000

    logging

--- FLF/model/TorchResNetFactory.py
+++ FLF/model/TorchResNetFactory.py
@@ -1,6 +1,8 @@
 from torch import nn
 import torchvision.models as models
 
+import logging
+
 
 class TorchResNetFactory:
     def __init__(self, norm="batch", init=None):
@@ -10,15 +12,18 @@ class TorchResNetFactory:
     def __call__(self):
         if self.norm == "batch":
             model = models.resnet18()
+            logging.info("ResNet keras batchnorm")
         elif self.norm == "group":
             make_group_norm = lambda x: nn.GroupNorm(2, x)
             model = models.resnet18(norm_layer=make_group_norm)
+            logging.info("ResNet keras groupnorm")
         else:
             raise Exception("NORM is not supported!")
 
         if self.init is not None:
             if self.init == "keras":
                 model = model.apply(self.__keras_like_init)
+                logging.info("ResNet keras initialized")
             else:
                 raise Exception("INIT is not supported!")
         return model
