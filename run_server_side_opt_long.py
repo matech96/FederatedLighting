6a45169514e1d59d2c906e186ff78e90e56565e4
Ákos Gángoly <gangoly96@gmail.com>
2020-08-07 06:48:38 +0000

    get_args training

--- run_server_side_opt_long.py
+++ run_server_side_opt_long.py
@@ -1,4 +1,5 @@
 from comet_ml import Experiment
+import torch as th
 
 import logging
 from FLF.TorchFederatedLearnerCIFAR100 import (
@@ -6,6 +7,15 @@ from FLF.TorchFederatedLearnerCIFAR100 import (
     TorchFederatedLearnerCIFAR100Config,
 )
 from FLF.BreakedTrainingExcpetion import ToLargeLearningRateExcpetion
+
+
+def get_args(opt):
+    if opt == "Adam":
+        return {"betas": (0.0, 0.99), "eps": 0.01}
+    elif opt == "SGD":
+        return {"momentum": 0.9}
+    else:
+        return {}
 
 
 def do_training(config: TorchFederatedLearnerCIFAR100Config):
@@ -17,6 +27,7 @@ def do_training(config: TorchFederatedLearnerCIFAR100Config):
     learner.train()
 
 
+th.cuda.set_device(3)
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
     level=logging.INFO,
@@ -32,45 +43,53 @@ E = 1
 B = 20
 is_iid = False
 # server_lr = 0.1
-server_opt = "SGD"
-client_opt = "SGD"
+# server_opt = "AdamW"
+# client_opt = "AdamW"
 client_opt_strategy = "avg"
 # image_norm = "tflike"
-wrong_lrs = []
-for server_lr in [0.01, 0.1, 1, 10]:
-    for client_lr in [0.001, 0.0001]:
-        if (server_lr != 10) and (client_lr == 0.001):
-            continue
+for server_opt in ["Adagrad", "Adam", "SGD"]:
+    client_opt = "SGD"  # ["Adagrad", "Adam", "SGD"]
+    if client_opt == server_opt:
+        continue
 
-        if any(
-            [((wslr <= server_lr) and (wclr <= client_lr)) for wslr, wclr in wrong_lrs]
-        ):
-            continue
+    wrong_lrs = []
+    for server_lr in [0.001, 0.01, 0.1, 1, 10]:
+        for client_lr in [0.0001, 0.001, 0.01, 0.1, 1]:
+            if any(
+                [
+                    (wslr <= server_lr) and (wclr <= client_lr)
+                    for wslr, wclr in wrong_lrs
+                ]
+            ):
+                continue
 
-        # TODO a paraméterek helytelen nevére nem adott hibát
-        config = TorchFederatedLearnerCIFAR100Config(
-            BREAK_ROUND=100,
-            CLIENT_LEARNING_RATE=client_lr,
-            CLIENT_OPT=client_opt,
-            # CLIENT_OPT_L2=1e-4,
-            CLIENT_OPT_ARGS={"momentum": 0.9},
-            CLIENT_OPT_STRATEGY=client_opt_strategy,
-            SERVER_OPT=server_opt,
-            # SERVER_OPT_ARGS={"betas": (0.0, 0.99), "eps": 0.01},
-            SERVER_OPT_ARGS={"momentum": 0.9},
-            SERVER_LEARNING_RATE=server_lr,
-            IS_IID_DATA=is_iid,
-            BATCH_SIZE=B,
-            CLIENT_FRACTION=C,
-            N_CLIENTS=NC,
-            N_EPOCH_PER_CLIENT=E,
-            MAX_ROUNDS=max_rounds,
-            DL_N_WORKER=0,
-            NORM="group",
-            # IMAGE_NORM=image_norm,
-            INIT="keras",
-        )
-        try:
-            do_training(config)
-        except ToLargeLearningRateExcpetion:
-            wrong_lrs.append((server_lr, client_lr))
+            # TODO a paraméterek helytelen nevére nem adott hibát
+            config = TorchFederatedLearnerCIFAR100Config(
+                BREAK_ROUND=300,
+                CLIENT_LEARNING_RATE=client_lr,
+                CLIENT_OPT=client_opt,
+                CLIENT_OPT_ARGS=get_args(client_opt),
+                # CLIENT_OPT_L2=1e-4,
+                # CLIENT_OPT_ARGS={"momentum": 0.9},
+                # CLIENT_OPT_ARGS={"betas": (0.0, 0.99), "eps": 0.01},
+                CLIENT_OPT_STRATEGY=client_opt_strategy,
+                SERVER_OPT=server_opt,
+                SERVER_OPT_ARGS=get_args(server_opt),
+                # SERVER_OPT_ARGS={"betas": (0.0, 0.99), "eps": 0.01},
+                # SERVER_OPT_ARGS={"momentum": 0.9},
+                SERVER_LEARNING_RATE=server_lr,
+                IS_IID_DATA=is_iid,
+                BATCH_SIZE=B,
+                CLIENT_FRACTION=C,
+                N_CLIENTS=NC,
+                N_EPOCH_PER_CLIENT=E,
+                MAX_ROUNDS=max_rounds,
+                DL_N_WORKER=0,
+                NORM="group",
+                # IMAGE_NORM=image_norm,
+                INIT="keras",
+            )
+            try:
+                do_training(config)
+            except ToLargeLearningRateExcpetion:
+                wrong_lrs.append((server_lr, client_lr))
