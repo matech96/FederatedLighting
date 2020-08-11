8ab0568e893e0ebbb2efa55dc750cdc26e231cc4
Ákos Gángoly <gangoly96@gmail.com>
2020-08-11 12:38:27 +0000

    run diff client opt

--- run_server_side_opt_long.py
+++ run_server_side_opt_long.py
@@ -27,7 +27,9 @@ def do_training(config: TorchFederatedLearnerCIFAR100Config):
     learner.train()
 
 
-th.cuda.set_device(3)
+run_id = 0
+free_gpu = [2, 3]
+th.cuda.set_device(free_gpu[run_id])
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
     level=logging.INFO,
@@ -42,48 +44,49 @@ NC = 500
 E = 1
 B = 20
 is_iid = False
-# server_lr = 0.1
+server_lr = 0.01
+client_lr = 0.001
 server_opt = "Adam"
-client_opt = "SGD"
-client_opt_strategy = "avg"
+client_opt = "Adam"
+# client_opt_strategy = "avg"
 # image_norm = "tflike"
 wrong_lrs = []
-for server_lr in [0.01, 0.1, 1, 10]:
-    for client_lr in [0.0001, 0.001, 0.01, 0.1, 1]:
-        if server_lr == 0.01 and client_lr < 0.1:
-            continue
+# for server_lr in [0.01, 0.1, 1, 10]:
+#     for client_lr in [0.0001, 0.001, 0.01, 0.1, 1]:
+#         if server_lr == 0.01 and client_lr < 0.1:
+#             continue
 
-        if any(
-            [
-                (wslr <= server_lr) and (wclr <= client_lr)
-                for wslr, wclr in wrong_lrs
-            ]
-        ):
-            continue
-
-        # TODO a paraméterek helytelen nevére nem adott hibát
-        config = TorchFederatedLearnerCIFAR100Config(
-            BREAK_ROUND=300,
-            CLIENT_LEARNING_RATE=client_lr,
-            CLIENT_OPT=client_opt,
-            CLIENT_OPT_ARGS=get_args(client_opt),
-            # CLIENT_OPT_L2=1e-4,
-            CLIENT_OPT_STRATEGY=client_opt_strategy,
-            SERVER_OPT=server_opt,
-            SERVER_OPT_ARGS=get_args(server_opt),
-            SERVER_LEARNING_RATE=server_lr,
-            IS_IID_DATA=is_iid,
-            BATCH_SIZE=B,
-            CLIENT_FRACTION=C,
-            N_CLIENTS=NC,
-            N_EPOCH_PER_CLIENT=E,
-            MAX_ROUNDS=max_rounds,
-            DL_N_WORKER=0,
-            NORM="group",
-            # IMAGE_NORM=image_norm,
-            INIT="keras",
-        )
-        try:
-            do_training(config)
-        except ToLargeLearningRateExcpetion:
-            wrong_lrs.append((server_lr, client_lr))
+#         if any(
+#             [
+#                 (wslr <= server_lr) and (wclr <= client_lr)
+#                 for wslr, wclr in wrong_lrs
+#             ]
+#         ):
+#             continue
+client_opt_strategy = ["reinit", "nothing", "avg"][run_id]
+# TODO a paraméterek helytelen nevére nem adott hibát
+config = TorchFederatedLearnerCIFAR100Config(
+    BREAK_ROUND=300,
+    CLIENT_LEARNING_RATE=client_lr,
+    CLIENT_OPT=client_opt,
+    CLIENT_OPT_ARGS=get_args(client_opt),
+    # CLIENT_OPT_L2=1e-4,
+    CLIENT_OPT_STRATEGY=client_opt_strategy,
+    SERVER_OPT=server_opt,
+    SERVER_OPT_ARGS=get_args(server_opt),
+    SERVER_LEARNING_RATE=server_lr,
+    IS_IID_DATA=is_iid,
+    BATCH_SIZE=B,
+    CLIENT_FRACTION=C,
+    N_CLIENTS=NC,
+    N_EPOCH_PER_CLIENT=E,
+    MAX_ROUNDS=max_rounds,
+    DL_N_WORKER=0,
+    NORM="group",
+    # IMAGE_NORM=image_norm,
+    INIT="keras",
+)
+try:
+    do_training(config)
+except ToLargeLearningRateExcpetion:
+    wrong_lrs.append((server_lr, client_lr))
