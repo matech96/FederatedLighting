38a75431cf485594d6ff7e80090eb991021be3a8
Ákos Gángoly <gangoly96@gmail.com>
2020-10-06 13:12:37 +0000

    config updates

--- run_500c1e1500r10f-yogi-n-yogi.py
+++ run_500c1e1500r10f-yogi-n-yogi.py
@@ -9,7 +9,7 @@ server_lr = 0.001
 client_lr = 0.01
 server_opt = "Yogi"
 client_opt = "Yogi"
-client_opt_strategy = "reinit"
+client_opt_strategy = "nothing"
 
 max_rounds = 1500
 n_clients_per_round = 10
