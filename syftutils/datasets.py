b67078de0ce350ddc5f6db246501fb62dcb61a16
Ákos Gángoly <gangoly96@gmail.com>
2020-10-10 17:10:19 +0000

    Revert "no syft util"
    This reverts commit 3a91e4cec04fc6259101c4ff325bd047a76a7e15.

---
+++ syftutils/datasets.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+import torch as th
+import syft as sy
+
+
+def get_dataset_items_at(ds, idx):
+    data = th.stack([ds[i][0] for i in idx])
+    target = ds.targets[idx]
+    return data, target
+
+
+class TorchClientBatchIter:
+    def __init__(self, federated_data_loader: sy.FederatedDataLoader):
+        self.federated_data_loader = federated_data_loader
+
+    def __iter__(self):
+        self.iter = iter(self.federated_data_loader)
+        self.client_curr_batch = defaultdict(lambda: 0)
+        return self
+
+    def __next__(self):
+        data, target = next(self.iter)
+        client_id = data.location
+        curr_batch = self.client_curr_batch[client_id]
+        self.client_curr_batch[client_id] += 1
+        return curr_batch, (data, target)
---
+++ syftutils/multipointer.py
@@ -0,0 +1,63 @@
+import torch as th
+
+
+def get_avg_results(ptr):
+    n_childs = len(ptr.child.child)
+    return ptr.get(sum_results=True) / n_childs
+
+
+def avg_models(models):
+    final_model = models[0].copy()
+
+    parameters = [model.state_dict() for model in models]
+    final_state_dict = avg_model_state_dicts(parameters)
+    final_model.load_state_dict(final_state_dict)
+
+    return final_model
+
+
+def avg_model_state_dicts(state_dicts):
+    final_state_dict = {}
+    with th.no_grad():
+        for parameter_name in state_dicts[0].keys():
+            if (not isinstance(state_dicts[0][parameter_name], th.Tensor)) or (
+                state_dicts[0][parameter_name].dtype == th.int64
+            ):
+                final_state_dict[parameter_name] = state_dicts[0][parameter_name]
+                # TODO assert equivalnce
+                continue
+
+            final_state_dict[parameter_name] = th.mean(
+                th.stack(
+                    [
+                        model_parameters[parameter_name]
+                        for model_parameters in state_dicts
+                    ]
+                ),
+                dim=0,
+            )
+    return final_state_dict
+
+
+def commulative_avg_models(model_0, model_1, n_models_0):
+    final_model = model_0.copy()
+
+    final_state_dict = commulative_avg_model_state_dicts(
+        model_0.state_dict(), model_1.state_dict(), n_models_0
+    )
+    final_model.load_state_dict(final_state_dict)
+
+    return final_model
+
+
+def commulative_avg_model_state_dicts(state_dict_0, state_dict_1, n_states_0):
+    if state_dict_0 is None:
+        return state_dict_1
+    final_state_dict = {}
+    with th.no_grad():
+        for parameter_name in state_dict_0.keys():
+            final_state_dict[parameter_name] = (
+                (state_dict_0[parameter_name] * n_states_0)
+                + state_dict_1[parameter_name]
+            ) / (n_states_0 + 1)
+    return final_state_dict
