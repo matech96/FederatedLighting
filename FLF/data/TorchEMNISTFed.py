92cac01c0b88dbd77bfe86a950da5f6d341dbc0c
matech96 <gangoly96@gmail.com>
2020-10-15 09:19:38 +0000

    cleanup

--- FLF/data/TorchEMNISTFed.py
+++ FLF/data/TorchEMNISTFed.py
@@ -26,7 +26,6 @@ class TorchEMNISTFed(Dataset):
             img = self.transform(self.images[idx, ])
         else:
             img = th.tensor(self.images[idx, ])
-        print(self.images.shape, img.shape)
 
         return img, th.tensor(self.labels[idx, ]).long()
 
