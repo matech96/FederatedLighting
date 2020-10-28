82e4f26dfe99ba3dc159a96946e8215911b77a2d
matech96 <gangoly96@gmail.com>
2020-10-28 21:05:57 +0000

    in vram opt

--- run_emnist_cnn_3400_avg.py
+++ run_emnist_cnn_3400_avg.py
@@ -57,7 +57,7 @@ for E in [1, 5, 10, 20, 30]:
                 MODEL=model,
             )
             config_technical = TorchFederatedLearnerTechnicalConfig(
-                BREAK_ROUND=300, EVAL_ROUND=10, TEST_LAST=20
+                BREAK_ROUND=300, EVAL_ROUND=10, TEST_LAST=20, STORE_OPT_ON_DISK=False
             )
             name = f"{config.SERVER_OPT}: {config.SERVER_LEARNING_RATE} - {config.CLIENT_OPT_STRATEGY} - {config.CLIENT_OPT}: {config.CLIENT_LEARNING_RATE}"
             experiment = Experiment(
