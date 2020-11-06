fa1ac552a30999beb7715b50ae6217dcabb7d42a
matech96 <gangoly96@gmai.com>
2020-11-06 09:43:18 +0000

    wtf

--- run_emnist_cnn_3400_sgdm.py
+++ run_emnist_cnn_3400_sgdm.py
@@ -22,8 +22,8 @@ model = "CNN"
 for _ in range(10):
     for _, m in common.get_besr_lrs_from_exps(server_opt, client_opt_strategy[0], client_opt).iterrows():
         E = m["E"]
-        client_lr = m["clr"]
-        server_lr = m["slr"]
+        server_lr = 10 ** s["slr"]
+        client_lr = 10 ** s["clr"]
         project_name = f"{model}{NC}c{E}e{max_rounds}r{n_clients_per_round}f-{server_opt}-{client_opt_strategy[0]}-{client_opt}-compare-best-lr"
         config = TorchFederatedLearnerEMNISTConfig(
             CLIENT_LEARNING_RATE=client_lr,
