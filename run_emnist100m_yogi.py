3e2e7797103f5ff3a6c01243ebc35f9a3548ee95
matech96 <gangoly96@gmai.com>
2020-11-13 14:14:37 +0000

    fedyogi m scf

--- run_emnist100m_yogi.py
+++ run_emnist100m_yogi.py
@@ -25,41 +25,45 @@ is_iid = False
 model = "CNN"
 E = 1
 
-project_name = f"{model}{NC}c{E}e{max_rounds}r{n_clients_per_round}f-{server_opt}-{client_opt_strategy[0]}-{client_opt}"
+project_name = f"{model}{NC}c{E}e{max_rounds}r{n_clients_per_round}f-{server_opt}-{client_opt_strategy[0]}-{client_opt}-scf"
 
-for client_lr_lg in np.arange(-2.0, 0.5, 0.5):
-    client_lr = 10 ** client_lr_lg
-    for server_lr_lg in np.arange(-2.5, 0.0, 0.5):
-        server_lr = 10 ** server_lr_lg
-        config = TorchFederatedLearnerEMNISTConfig(
-            CLIENT_LEARNING_RATE=client_lr,
-            CLIENT_OPT=common.get_name(client_opt),
-            CLIENT_OPT_ARGS=common.get_args(client_opt),
-            # CLIENT_OPT_L2=1e-4,
-            CLIENT_OPT_STRATEGY=client_opt_strategy,
-            SERVER_OPT=common.get_name(server_opt),
-            SERVER_OPT_ARGS=common.get_args(server_opt),
-            SERVER_LEARNING_RATE=server_lr,
-            IS_IID_DATA=is_iid,
-            BATCH_SIZE=B,
-            CLIENT_FRACTION=C,
-            N_CLIENTS=NC,
-            N_EPOCH_PER_CLIENT=E,
-            MAX_ROUNDS=max_rounds,
-            MODEL=model,
-        )
-        config_technical = TorchFederatedLearnerTechnicalConfig(
-            BREAK_ROUND=300,
-            EVAL_ROUND=1,
-            TEST_LAST=1,
-            STORE_OPT_ON_DISK=False,
-            STORE_MODEL_IN_RAM=False,
-        )
-        name = f"{config.SERVER_OPT}: {config.SERVER_LEARNING_RATE} - {config.CLIENT_OPT_STRATEGY} - {config.CLIENT_OPT}: {config.CLIENT_LEARNING_RATE}"
-        experiment = Experiment(
-            workspace="federated-learning-emnistlm", project_name=project_name
-        )
-        try:
-            common.do_training_emnist(experiment, name, config, config_technical)
-        except ToLargeLearningRateExcpetion:
-            pass
+client_lr_lg = -1
+server_lr_lg = -1
+
+# for client_lr_lg in np.arange(-2.0, 0.5, 0.5):
+client_lr = 10 ** client_lr_lg
+# for server_lr_lg in np.arange(-2.5, 0.0, 0.5):
+server_lr = 10 ** server_lr_lg
+config = TorchFederatedLearnerEMNISTConfig(
+    CLIENT_LEARNING_RATE=client_lr,
+    CLIENT_OPT=common.get_name(client_opt),
+    CLIENT_OPT_ARGS=common.get_args(client_opt),
+    # CLIENT_OPT_L2=1e-4,
+    CLIENT_OPT_STRATEGY=client_opt_strategy,
+    SERVER_OPT=common.get_name(server_opt),
+    SERVER_OPT_ARGS=common.get_args(server_opt),
+    SERVER_LEARNING_RATE=server_lr,
+    IS_IID_DATA=is_iid,
+    BATCH_SIZE=B,
+    CLIENT_FRACTION=C,
+    N_CLIENTS=NC,
+    N_EPOCH_PER_CLIENT=E,
+    MAX_ROUNDS=max_rounds,
+    MODEL=model,
+    SCAFFOLD=True,
+)
+config_technical = TorchFederatedLearnerTechnicalConfig(
+    BREAK_ROUND=300,
+    EVAL_ROUND=1,
+    TEST_LAST=1,
+    STORE_OPT_ON_DISK=False,
+    STORE_MODEL_IN_RAM=False,
+)
+name = f"{config.SERVER_OPT}: {config.SERVER_LEARNING_RATE} - {config.CLIENT_OPT_STRATEGY} - {config.CLIENT_OPT}: {config.CLIENT_LEARNING_RATE}"
+experiment = Experiment(
+    workspace="federated-learning-emnistlm", project_name=project_name
+)
+try:
+    common.do_training_emnist(experiment, name, config, config_technical)
+except ToLargeLearningRateExcpetion:
+    pass
